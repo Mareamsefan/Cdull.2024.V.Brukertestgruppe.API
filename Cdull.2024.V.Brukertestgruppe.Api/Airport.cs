ed38ca276aec56b15b3840109c51cb14b39d0fbe
ChrisBakk <Christina.aasbakk@live.no>
2024-02-28 15:19:51 +0100

    Changes in the following classes: Airport.cs and Gate.cs.

--- Cdull.2024.V.Brukertestgruppe.Api/Airport.cs
+++ Cdull.2024.V.Brukertestgruppe.Api/Airport.cs
@@ -1,7 +1,30 @@
-﻿namespace Cdull._2024.V.Brukertestgruppe.Api
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Cdull._2024.V.Brukertestgruppe.Api
 {
     public class Airport
     {
 
+        private List<Gate> Gates { get; set; } = new List<Gate>();  
+
+        private List<Runway> Runaways { get; set; } = new List<Runway>();
+
+        private List<Taxiway> Taxiways { get; set; } = new  List<Taxiway>();
+
+        public void ConfigurateAirPort(List<Gate> gates, List<Runway> runways, List<Taxiway> taxiways);
+
+        public override string ToString()
+        {
+            return null;
+        }
+
+
+
+
+
+
+
     }
+
+    
 }
