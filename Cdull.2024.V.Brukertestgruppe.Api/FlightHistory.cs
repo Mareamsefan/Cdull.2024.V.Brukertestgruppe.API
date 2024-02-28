8204a9f1684b5e564c215d72d428d839c52fade8
kriss <kristeno@hiof.no>
2024-02-28 15:19:46 +0100

    flighthistory og taxiway.

--- Cdull.2024.V.Brukertestgruppe.Api/FlightHistory.cs
+++ Cdull.2024.V.Brukertestgruppe.Api/FlightHistory.cs
@@ -8,5 +8,18 @@ namespace Cdull._2024.V.Brukertestgruppe.Api
 {
     internal class FlightHistory
     {
+        private string FlightNumber { get; set; }
+        private List<string> UsedGates { get; set; } = new List<string>();
+        private List<string> UsedRunways { get; set; } = new List<string>();
+        private DateTime Timestamps { get; set; }
+
+        private List<FlightHistory> allFlightHistories;
+        public FlightHistory(string FlightNumber, List<string> UsedGates, List<string> UsedRunways, DateTime TimeStamps);
+
+        public void CreateFlightHistory(string FlightNumber, List<string> UsedGates, List<string> UsedRunways, DateTime TimeStamps); 
+
+        public List<string> GetFlightHistory(string FlightNumber);
+
+        public string ToString();
     }
 }
